d1699e5fe881b2c44248c4f26f1276cea36f16f6
Marvin Zwolsman <mzwolsman@bol.com>
2021-11-30 11:06:39 +0100

    chore: add day01 package

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,3 +1,7 @@
+package day01
+
+import readInput
+
 fun main() {
     fun part1(input: List<String>): Int {
         return input.size
@@ -8,10 +12,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("day01/test")
     check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput("day01/input")
     println(part1(input))
     println(part2(input))
 }
