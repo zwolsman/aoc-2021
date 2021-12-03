f55c47acace797612ee695ed64a96a24296c3eb9
Marvin Zwolsman <mzwolsman@bol.com>
2021-12-03 21:33:13 +0100

    chore: prep day 4

---
+++ src/day04/Day04.kt
@@ -0,0 +1,24 @@
+package day04
+
+import readInput
+
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("day04/test")
+    check(part1(testInput) == 0) { "result = ${part1(testInput)}" }
+    check(part2(testInput) == 0) { "result = ${part2(testInput)}" }
+
+    val input = readInput("day04/input")
+    println(part1(input))
+    println(part2(input))
+}
