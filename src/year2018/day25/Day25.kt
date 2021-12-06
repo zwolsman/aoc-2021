3792f1b8022b7c36059d21bb1500a17fe2eba4b7
Marvin Zwolsman <mzwolsman@bol.com>
2021-12-06 17:32:22 +0100

    chore: prep year 2018

---
+++ src/year2018/day25/Day25.kt
@@ -0,0 +1,23 @@
+package year2018.day25
+
+import readInput
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("year2018/day25/test")
+    check(part1(testInput) == 1) { "result = ${part1(testInput)}" }
+    check(part2(testInput) == 1) { "result = ${part2(testInput)}" }
+
+    val input = readInput("year2018/day25/input")
+    println(part1(input))
+    println(part2(input))
+}
