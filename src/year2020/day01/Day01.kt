a7f642fc26dc70683d30911ca3e98b72b08754dd
Marvin Zwolsman <mzwolsman@bol.com>
2021-12-06 16:38:23 +0100

    task: 2020 day 1

--- src/year2020/day01/Day01.kt
+++ src/year2020/day01/Day01.kt
@@ -5,17 +5,35 @@ import readInput
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return input.size
+        val expenseReport = input.map { it.toInt() }
+        for (i in expenseReport) {
+            for (j in expenseReport) {
+                if (i + j == 2020)
+                    return i * j
+            }
+        }
+
+        error("no pair found")
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val expenseReport = input.map { it.toInt() }
+        for (i in expenseReport) {
+            for (j in expenseReport) {
+                for (k in expenseReport) {
+                    if (i + j + k == 2020)
+                        return i * j * k
+                }
+            }
+        }
+
+        error("no triplet found")
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("year2020/day01/test")
-    check(part1(testInput) == 1) { "result = ${part1(testInput)}" }
-    check(part2(testInput) == 1) { "result = ${part2(testInput)}" }
+    check(part1(testInput) == 514579) { "result = ${part1(testInput)}" }
+    check(part2(testInput) == 241861950) { "result = ${part2(testInput)}" }
 
     val input = readInput("year2020/day01/input")
     println(part1(input))
