72b0d60741ced726e45b58990fe8c9a88e857ec5
Marvin Zwolsman <mzwolsman@bol.com>
2021-12-06 06:18:20 +0100

    task: day 6 part 1

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -5,7 +5,19 @@ import readInput
 fun main() {
 
     fun part1(input: List<String>): Int {
-       return input.size
+        var fish = input.map { it.toInt() }
+        for (day in 0 until 80) {
+            val a = fish
+                .map { it - 1 }
+                .toMutableList()
+
+            val newFish = a.count { it == -1 }
+            a.removeAll { it == -1 }
+
+            fish = a + List(newFish) { 6 } + List(newFish) { 8 }
+        }
+
+        return fish.size
     }
 
     fun part2(input: List<String>): Int {
@@ -13,11 +25,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("day06/test")
-    check(part1(testInput) == 1) { "result = ${part1(testInput)}" }
-    check(part2(testInput) == 1) { "result = ${part2(testInput)}" }
+    val testInput = readInput("day06/test", ",")
+    check(part1(testInput) == 5934) { "result = ${part1(testInput)}" }
+//    check(part2(testInput) == 1) { "result = ${part2(testInput)}" }
 
-    val input = readInput("day06/input")
+    val input = readInput("day06/input", ",")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
