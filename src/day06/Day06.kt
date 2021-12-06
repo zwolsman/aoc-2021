6217adadaa50b9850d8b3a3d25c65ff18ade8649
Marvin Zwolsman <mzwolsman@bol.com>
2021-12-06 09:51:32 +0100

    refactor: day 6

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -5,24 +5,27 @@ import readInput
 const val NEW_FISH = 8
 const val RESET_FISH = 6
 fun main() {
-    fun simulateFish(initialState: List<Int>, days: Int): Long {
-        var state = initialState.groupBy { it }.mapValues { (_, v) -> v.size.toLong() }
+    fun simulateFish(input: List<Int>, days: Int): Long {
+        val initialState = input.groupBy { it }.mapValues { (_, v) -> v.size.toLong() }
+        val daysRange = 0 until days
 
-        for (day in 0 until days) {
-            val newState = mutableMapOf<Int, Long>()
-            for ((age, count) in state) {
-                if (age == 0) {
-                    newState[NEW_FISH] = count
-                    newState[RESET_FISH] = newState.getOrDefault(RESET_FISH, 0) + count
-                } else {
-                    val key = age - 1
+        return daysRange
+            .fold(initialState) { state, _ ->
+                val newState = mutableMapOf<Int, Long>()
+                state.forEach { (day, count) ->
+                    val key = if (day == 0) {
+                        newState[NEW_FISH] = count
+                        RESET_FISH
+                    } else {
+                        day - 1
+                    }
+
                     newState[key] = newState.getOrDefault(key, 0) + count
                 }
-                state = newState
+                newState
             }
-        }
-
-        return state.values.sum()
+            .values
+            .sum()
     }
 
     fun part1(input: List<String>): Long {
@@ -38,9 +41,11 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day06/test", ",")
     check(part1(testInput) == 5934L) { "result = ${part1(testInput)}" }
-    check(part2(testInput) == 26984457539) { "result = ${part2(testInput)}" }
+    check(part2(testInput) == 26984457539L) { "result = ${part2(testInput)}" }
 
     val input = readInput("day06/input", ",")
+    check(part1(input) == 345793L)
+    check(part2(input) == 1572643095893L)
     println(part1(input))
     println(part2(input))
 }
