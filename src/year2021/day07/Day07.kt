64e4858e882005237520f06f49bf51cd5eaf6bd1
Marvin Zwolsman <mzwolsman@bol.com>
2021-12-07 06:12:53 +0100

    task: 2021 day 7 part 1

--- src/year2021/day07/Day07.kt
+++ src/year2021/day07/Day07.kt
@@ -1,11 +1,20 @@
 package year2021.day07
 
 import readInput
+import java.lang.Math.abs
 
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return input.size
+        val crabs = input.map { it.toInt() }.sorted()
+        val fuelMap = mutableMapOf<Int, Int>()
+
+        for (target in 0 until crabs.maxOf { it }) {
+            fuelMap[target] = crabs.fold(0) { acc, crab ->
+                acc + abs(crab - target)
+            }
+        }
+        return fuelMap.minOf { (_, costs) -> costs }
     }
 
     fun part2(input: List<String>): Int {
@@ -13,11 +22,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("year2021/day07/test")
-    check(part1(testInput) == 1) { "result = ${part1(testInput)}" }
-    check(part2(testInput) == 1) { "result = ${part2(testInput)}" }
+    val testInput = readInput("year2021/day07/test", ",")
+    check(part1(testInput) == 37) { "result = ${part1(testInput)}" }
+//    check(part2(testInput) == 1) { "result = ${part2(testInput)}" }
 
-    val input = readInput("year2021/day07/input")
+    val input = readInput("year2021/day07/input", ",")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
