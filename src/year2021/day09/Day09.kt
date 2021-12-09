b45041ed0447a9e8a33fa096510a3b4e36d584f1
Marvin Zwolsman <mzwolsman@bol.com>
2021-12-09 10:25:49 +0100

    task: 2021 day 9 part 1

--- src/year2021/day09/Day09.kt
+++ src/year2021/day09/Day09.kt
@@ -5,7 +5,17 @@ import readInput
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return input.size
+        val width = input.first().length
+        val cave = input.flatMap { line -> line.split("").mapNotNull { height -> height.toIntOrNull() } }
+        val options = listOf(-1, 1, -width, width)
+
+        val lowPoints = cave
+            .filterIndexed { index, height ->
+                val adjacentLocations = options.mapNotNull { option -> cave.getOrNull(option + index) }
+
+                adjacentLocations.all { height < it }
+            }
+        return lowPoints.sumOf { it + 1 }
     }
 
     fun part2(input: List<String>): Int {
@@ -14,10 +24,10 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("year2021/day09/test")
-    check(part1(testInput) == 1) { "result = ${part1(testInput)}" }
-    check(part2(testInput) == 1) { "result = ${part2(testInput)}" }
+    check(part1(testInput) == 15) { "result = ${part1(testInput)}" }
+//    check(part2(testInput) == 1) { "result = ${part2(testInput)}" }
 
     val input = readInput("year2021/day09/input")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
