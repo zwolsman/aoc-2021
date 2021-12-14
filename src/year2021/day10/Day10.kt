8fb7a2d70350e322f8d3bc878c25ca78afecce51
Marvin Zwolsman <mzwolsman@bol.com>
2021-12-14 19:51:50 +0100

    task: 2021 day 10 part 1

--- src/year2021/day10/Day10.kt
+++ src/year2021/day10/Day10.kt
@@ -4,8 +4,42 @@ import readInput
 
 fun main() {
 
+    val pairs = mapOf(
+        '(' to ')',
+        '[' to ']',
+        '{' to '}',
+        '<' to '>',
+    )
+
+    val points = mapOf(
+        ')' to 3,
+        ']' to 57,
+        '}' to 1197,
+        '>' to 25137
+    )
+
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val missing = mutableMapOf<Char, Int>()
+        root@ for (line in input) {
+            val stack = mutableListOf<Char>()
+
+            for (c in line) {
+                if (c in pairs.keys)
+                    stack += c
+                if (c in pairs.values) {
+                    val last = stack.removeLastOrNull()
+                    val expected = pairs[last]
+                    if (expected != c) {
+                        missing[c] = missing.getOrDefault(c, 0) + 1
+                        continue@root
+                    }
+                }
+
+            }
+        }
+
+        return missing.map { (key, amount) -> points[key]!! * amount }.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -14,10 +48,10 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("year2021/day10/test")
-    check(part1(testInput) == 1) { "result = ${part1(testInput)}" }
-    check(part2(testInput) == 1) { "result = ${part2(testInput)}" }
+    check(part1(testInput) == 26397) { "result = ${part1(testInput)}" }
+//    check(part2(testInput) == 1) { "result = ${part2(testInput)}" }
 
     val input = readInput("year2021/day10/input")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
